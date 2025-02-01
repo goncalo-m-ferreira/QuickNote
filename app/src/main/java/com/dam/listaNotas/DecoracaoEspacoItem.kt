fda45e2e17a1e251e6eebbc9a327a9883484e505
Pedro Afonso Mesquita Cortesão <85261682+CallMeDefective@users.noreply.github.com>
2025-02-01 19:05:00 +0000

    Criação da classe 'DecoracaoEspacoItem'

---
+++ app/src/main/java/com/dam/listaNotas/DecoracaoEspacoItem.kt
@@ -0,0 +1,26 @@
+package com.dam.listaNotas
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DecoracaoEspacoItem(context: Context) : RecyclerView.ItemDecoration() {
+
+    // Espaçamento entre os itens da lista na vertical e horizontal para a RecyclerView
+    private val espacoVert = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, 7f, context.resources.displayMetrics
+    ).toInt()
+    private val espacoHorz = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, 4f, context.resources.displayMetrics
+    ).toInt()
+
+    // Define o espaçamento entre os itens da lista na vertical e horizontal para a RecyclerView
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = -espacoHorz
+        outRect.right = -espacoHorz
+        outRect.top = -espacoVert
+        outRect.bottom = -espacoVert
+    }
+}
